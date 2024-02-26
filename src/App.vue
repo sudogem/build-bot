8fa19bcf7d0f1d0772a889dacadfc74ac2e64b09
sudogem <sudogem@gmail.com>
2024-02-26 22:23:53 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<!-- eslint-disable vuejs-accessibility/alt-text -->
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" />Build a bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <RobotBuilder />
+  </main>
+</template>
+
+<script>
+// import HomePage from './components/home/HomePage.vue';
+import RobotBuilder from './components/build/RobotBuilder.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RobotBuilder,
+  },
+};
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+main {
+  margin:0 auto;
+  padding: 30px;
+  background: #fff;
+  width: 1024px   ;
+  min-height: 300px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+
+</style>
---
+++ src/components/home/HomePage.vue
@@ -0,0 +1,44 @@
+<!-- eslint-disable vuejs-accessibility/alt-text -->
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../../assets/robot-home.png" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
