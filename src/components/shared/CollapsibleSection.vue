b285ac639c65a1d8d04579ab30b008edb9d67c7c
sudogem <sudogem@gmail.com>
2024-03-01 21:09:08 +0800

    injecting content into child component with slot

---
+++ src/components/shared/CollapsibleSection.vue
@@ -0,0 +1,31 @@
+<!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="open = !open" class="toggled">Collapse</span>
+      <span v-if="!open" @click="open = !open" class="toggled">Expand</span>
+    </div>
+    <slot v-if="open"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return { open: true };
+  },
+};
+</script>
+
+<style scoped>
+  .header {
+    background-color: #ccc;
+    padding: 3px;
+    cursor: pointer;
+  }
+  .toggled {
+    width: 100%;
+    display: block;
+  }
+</style>
