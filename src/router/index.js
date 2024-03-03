f53311a534dbe6e277bd9c3187ea0d6fc2908017
sudogem <sudogem@gmail.com>
2024-03-03 23:23:37 +0800

    using nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ import {
 import HomePage from '../components/home/HomePage.vue';
 import RobotBuilder from '../components/build/RobotBuilder.vue';
 import PartInfo from '../components/part/PartInfo.vue';
+import BrowsePart from '../components/part/BrowsePart.vue';
+import RobotHead from '../components/part/RobotHead.vue';
+import RobotArms from '../components/part/RobotArms.vue';
+import RobotTorsos from '../components/part/RobotTorsos.vue';
+import RobotBases from '../components/part/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -17,6 +22,32 @@ export default createRouter({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  }, {
+    path: '/parts/browse',
+    name: 'Browse Parts',
+    component: BrowsePart,
+    children: [
+      {
+        name: 'BrowseHead',
+        path: 'heads',
+        component: RobotHead,
+      },
+      {
+        name: 'BrowseArm',
+        path: 'arms',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseTorso',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+      {
+        name: 'BrowseBase',
+        path: 'bases',
+        component: RobotBases,
+      },
+    ],
   }, {
     path: '/parts/:partType/:id',
     name: 'Part Info',
